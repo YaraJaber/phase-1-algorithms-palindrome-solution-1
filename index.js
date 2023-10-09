5e1cebcc30174c1e73546406bf6e28b0db5b0146
YaraJaber <145011720+YaraJaber@users.noreply.github.com>
2023-10-09 21:08:47 +0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,18 +1,56 @@
 function isPalindrome(word) {
   // Write your algorithm here
 }
+function isPalindrome(word) {
+  function isLetter(char) {
+    return /[a-z]/.test(char);
+  }
 
-/* 
-  Add your pseudocode here
-*/
+  const cleanedWord = word.toLowerCase().split('').filter(isLetter).join('');
 
-/*
-  Add written explanation of your solution here
-*/
+  let start = 0;
+  let end = cleanedWord.length - 1;
 
-// You can run `node index.js` to view these console logs
+  while (start < end) {
+    if (cleanedWord[start] !== cleanedWord[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+
+  return true;
+}
+
+console.log("Expecting: true");
+console.log("=>", isPalindrome("racecar"));
+
+console.log("");
+
+console.log("Expecting: true");
+console.log("=>", isPalindrome("mom"));
+
+console.log("");
+
+console.log("Expecting: true");
+console.log("=>", isPalindrome("abba"));
+
+console.log("");
+
+console.log("Expecting: true");
+console.log("=>", isPalindrome("a"));
+
+console.log("");
+
+console.log("Expecting: false");
+console.log("=>", isPalindrome("hi"));
+
+console.log("");
+
+console.log("Expecting: false");
+console.log("=>", isPalindrome("robot"));
+
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
